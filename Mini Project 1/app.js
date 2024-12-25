58c0f9349d1119182f709b973b2cfef28652b70c
invtfl-bharti <bhartijayprakash19@gmail.com>
2024-12-25 10:36:16 +0530

    files added

--- Mini Project 1/app.js
+++ Mini Project 1/app.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userModel = require('./models/user');
-const cookieParser = require('cookie-parser');
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const user = require("./models/user");
 
 // middlewares
 app.set("view engine", "ejs");
@@ -9,8 +13,61 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', function (req, res) {
-    res.render("index");
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.post("/register", async function (req, res) {
+  let { email, username, password, age, name } = req.body;
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(500).send("User already registered");
+
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
+      // create a new user in the database with the hashed password
+      let user = await userModel.create({
+        username,
+        email,
+        age,
+        name,
+        password: hash,
+      });
+
+      // create a JWT token for the newly registered user
+      let token = jwt.sign({ email: email, userid: user._id }, "shhhhh");
+
+      res.cookie("token", token);
+      res.send("Registered");
+    });
+  });
+});
+
+app.get("/login", isLoggedIn, (req, res) {
+  res.render("login");
+});
+
+app.post("/login", async function (req, res) {
+  let { email, password } = req.body;
+
+  // check if the password matches the existing password
+
+  let user = await userModel.findOne({ email });
+  if (!user) return res.status(500).send("User doesn't exist");
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) res.status(200).send("You can login now");
+    else res.redirect("/login");
+  });
+});
+
+
+app.get("/logout", function (req, res) {
+    res.cookie("token", "");
+    res.render("login");
 })
 
+// middleware for protected routes
+function isLoggedIn(req, res, next) {
+    if (req.cookies.token === "") res.send("You must be logged in");
+    next();
+}
 app.listen(3000);
--- Mini Project 1/models/user.js
+++ Mini Project 1/models/user.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject1");
 
 const userSchema = mongoose.Schema({
-    username: String,
-    email: String,
-    name: String,
-    password: String,
-    age:Number
-})
+  username: String,
+  name: String,
+  age: Number,
+  email: String,
+  password: String,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post " }],
+});
 
-module.exports = mongoose.model('user', userSchema);
+
+module.exports = mongoose.model("user", userSchema);
