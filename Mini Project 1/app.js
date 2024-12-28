2c7b1e5161647c7b2891eb82cedb57d58b77e02c
invtfl-bharti <bhartijayprakash19@gmail.com>
2024-12-29 04:05:30 +0530

    files added

--- Mini Project 1/app.js
+++ Mini Project 1/app.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require('path');
 const userModel = require('./models/user');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 
 
 // middlewares
-app.set("viee engine", "ejs");
+app.set("view engine", "ejs");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -14,5 +16,25 @@ app.use(cookieParser());
 app.get("/", function (req, res) {
     res.render("index");
 });
+
+app.post("/register", async function (req, res) {
+    let { email, username, age, name } = req.body;
+    let user = await userModel.findOne({ email });
+    if (user) {
+        return res.status(500).send("User already registered");
+    }
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+            let user = await userModel.create({
+                username,
+                email,
+                name,
+                age,
+                password:hash
+            })
+        }) 
+    })
+
+})
 
 app.listen(3000);
