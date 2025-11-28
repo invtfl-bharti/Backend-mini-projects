80666a8327e0831f390df56ff41426c75037c569
Bharti Jayprakash <bhartijayprakash19@gmail.com>
2025-11-29 02:39:28 +0530

    Controller and Routes successfully created

---
+++ AlgoCode-Backend/src/controllers/problem.controller.js
@@ -0,0 +1,27 @@
+function pingController(req, res) {
+    return res.json({message:'Ping controller is up.'})
+}
+
+function addProblem(req, res) {
+
+}
+
+function getProblem(req, res) {
+
+}
+
+
+function deleteProblem(req, res) {
+
+}
+
+function updateProblem(req, res) {
+
+}
+module.exports = {
+    addProblem,
+    getProblem,
+    deleteProblem,
+    updateProblem,
+    pingController
+}
--- AlgoCode-Backend/src/index.js
+++ AlgoCode-Backend/src/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const {PORT} = require('./config/server.config')
 const app = express();
+const apiRouter = require('./routes');
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
     return res.json({message:'Problem Service is alive'});
